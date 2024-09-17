ecc2bbeeddb4694b13532ae56c070ad64f0af6a4
Maksim Lebedev <monk527@yandex.ru>
2024-09-17 14:59:01 +0500

    09_mutability

---
+++ src/09_mutability/09_01.test.ts
@@ -0,0 +1,24 @@
+function icreaseAge(u: UserType) {
+  u.age++;
+}
+
+type UserType = {
+  name: string;
+  age: number;
+};
+
+test("array test", () => {
+  var user: UserType = {
+    name: "Dimych",
+    age: 32,
+  };
+
+  icreaseAge(user);
+
+  const superman = user
+  superman.age = 1000
+
+  expect(user.age).toBe(1000);
+
+  
+});
